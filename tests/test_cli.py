9255a6d80927ce504f8f213b977cc3480947642b
Pablo Valverde <92272697+pavalso@users.noreply.github.com>
2023-01-26 16:59:53 +0100

    Add test for CLI main

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,7 @@ import src.pypassword.cli as cli
 from click.testing import CliRunner
 
 
-cli_runner = CliRunner()
+runner = CliRunner()
 
 @pytest.mark.parametrize(
     'cmd',
@@ -22,7 +22,47 @@ cli_runner = CliRunner()
 def test_existance(cmd):
     # Add --help at the end in order to avoid executing the command but keep checking if the arguments exists
     aux_cmd = cmd + ['--help']
-    out = cli_runner.invoke(cli.main, aux_cmd)
+    result = runner.invoke(cli.main, aux_cmd)
     
     # If an argument didn't exists an exit code of 2 is raised
-    assert out.exit_code == 0, "Missing '%s'" % ' '.join(cmd)
+    assert result.exit_code == 0, "Missing '%s'" % ' '.join(cmd)
+
+def test_main_command():
+    # Test that the password is returned when the mode is 'random' and length is 12
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['random'])
+    assert result.exit_code == 0
+    assert len(result.output) == 12
+
+    # Test that the password is returned when the mode is 'random' and length is specified
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['random', '-l', '20'])
+    assert result.exit_code == 0
+    assert len(result.output) == 20
+
+    # Test that the password is returned when the mode is 'random' and the characters are specified in a file
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open('chars.txt', 'w') as f:
+            f.write('abc')
+        result = runner.invoke(cli.main, ['random', '-C', 'chars.txt'])
+    assert result.exit_code == 0
+    assert set(result.output[:-1]) <= {'a','b','c'}
+
+    # Test that the password is returned when the mode is 'random' and the characters are specified as a string
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['random', '-c', 'abc'])
+    assert result.exit_code == 0
+    assert set(result.output[:-1]) <= {'a','b','c'}
+
+    # Test that the help message is returned if the mode is not specified
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'Command-line tool for generating secure passwords' in result.output
+
+    # Test that the version is returned when the --version option is provided
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['--version'])
+    assert result.exit_code == 0
+    assert 'Pypassword' in result.output
