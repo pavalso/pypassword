124a07d76fc24142b8a5e1276a1f05af8f21d71b
Pablo Valverde <92272697+pavalso@users.noreply.github.com>
2023-01-26 13:07:36 +0100

    Check arguments

--- src/pypassword/password.py
+++ src/pypassword/password.py
@@ -7,7 +7,9 @@ def create_password(
 ) -> bytes:
     """Gets `length` random bytes from `available_characters` and create a new bytes sequence from it
     """
-    if not length or not available_characters:
+    if length < 0:
+        raise RuntimeError(f'Length must be positive not {length}')
+    if not available_characters:
         return b''
     list_characters = list(available_characters)
     buffer = [random.choice(list_characters) for _ in range(length)]
